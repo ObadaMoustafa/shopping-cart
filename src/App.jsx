8130d7a3783f27c0e0f44c3133b90235a3052322
unknown <obadamoustafa@gmail.com>
2022-10-12 19:32:59 +0200

    firebase initailized

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { Box, Button } from "@mui/material";
 import Navbar from "./components/navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { ref, set, update } from "firebase/database";
+import { db } from "./firebase/firebase";
 
 function App() {
+  useEffect(() => {
+    update(ref(db, "carts"), {
+      productName: "keyboard",
+    });
+  }, []);
   return (
     <Box>
       <Navbar />
